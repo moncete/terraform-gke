6dd08e4b2bff690ddee00cc2af5fae4f83f01c8a
moncete <moncete@gmail.com>
2019-10-14 10:25:29 +0200

    cambio nombre fichero .json

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("./crd/testbuild-252808-e13576bdffb8.json")}"
+  credentials = "${file("./crd/terraform.json")}"
   project     = var.project_id
   region      = var.region
 }
